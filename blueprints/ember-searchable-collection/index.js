c6aeab327f8cd28dd8ff0bf50dae4e5e17134319
Garrett Murphey <gmurphey@trovedata.com>
2016-05-17 14:52:30 -0400

    Fixing default blueprint.

--- blueprints/ember-searchable-collection/index.js
+++ blueprints/ember-searchable-collection/index.js
@@ -2,6 +2,10 @@
 module.exports = {
   description: '',
 
+  normalizeEntityName: function() {
+
+  },
+
   afterInstall: function(options) {
     return this.addPackageToProject('ember-one-way-controls');
   }
